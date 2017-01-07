d94a97f7338a4d9968b289b1963a9c59aeaa9bbc
Jason von Nieda <jason@vonnieda.org>
2017-01-07 14:45:37 -0600

    Frantz's version of loose part feeder. Not tested.

--- src/main/java/org/openpnp/machine/reference/feeder/ReferenceLoosePartFeeder.java
+++ src/main/java/org/openpnp/machine/reference/feeder/ReferenceLoosePartFeeder.java
@@ -19,6 +19,7 @@
 
 package org.openpnp.machine.reference.feeder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.Action;
@@ -30,6 +31,7 @@ import org.openpnp.gui.support.PropertySheetWizardAdapter;
 import org.openpnp.gui.support.Wizard;
 import org.openpnp.machine.reference.ReferenceFeeder;
 import org.openpnp.machine.reference.feeder.wizards.ReferenceLoosePartFeederConfigurationWizard;
+import org.openpnp.model.Footprint;
 import org.openpnp.model.Location;
 import org.openpnp.spi.Camera;
 import org.openpnp.spi.Nozzle;
@@ -54,6 +56,15 @@ import org.slf4j.LoggerFactory;
  * And related to that, this might be a good time to play with something like SIFT.
  */
 public class ReferenceLoosePartFeeder extends ReferenceFeeder {
+
+    // FCA the size of package is compared with a rectangle given by the camera.
+    // If percent of area is inside an interval of PERCENT_AREA, the rectangle is a good candidate
+    // The ratio of rectangle (length, with) is compared with the same way.
+    // @TODO put this datas inside a wizard.
+    private final static double PERCENT_AREA = 0.25;
+    private final static double PERCENT_RATIO = 0.30;
+
+
     private final static Logger logger = LoggerFactory.getLogger(ReferenceLoosePartFeeder.class);
 
     @Element(required = false)
@@ -77,28 +88,155 @@ public class ReferenceLoosePartFeeder extends ReferenceFeeder {
         }
     }
 
+    /**
+     * 
+     * @param results List of RotatedRectangle
+     * @param ratio Rectangle filter value, suppress of the returned list if not in the interval:
+     * @param areaPixels Area filter
+     * @return The new filtered list.
+     */
+    private List<RotatedRect> filterDimensionRotatedRect(List<RotatedRect> results, double ratio,
+            double areaPixels) {
+        List<RotatedRect> filteredResults = new ArrayList<RotatedRect>(results);
+        filteredResults.removeIf((a) -> {
+            // No dimension, suppress
+            if (a.size.height == 0 || a.size.width == 0) {
+                logger.debug("Size of footprint detected = 0");
+                return true;
+            }
+            double ratioC = Math.min(a.size.width / a.size.height, a.size.height / a.size.width);
+            double pcentRatio = Math.abs((ratioC - ratio) / ratio);
+            if (pcentRatio > PERCENT_RATIO) {
+                logger.debug("Ratio measured = " + ratioC + " waited=" + ratio
+                        + "... footprint suppressed");
+            }
+
+            return pcentRatio > PERCENT_RATIO;
+        });
+        filteredResults.removeIf((a) -> {
+            double pcent = Math.abs((a.size.area() - areaPixels) / areaPixels);
+            boolean result = pcent > PERCENT_AREA;
+            if (result) {
+                logger.debug("Area filtered error of" + (pcent * 100.0) + " %");
+            }
+            return result;
+        });
+        return filteredResults;
+    }
+
+    private double getBodyArea(Footprint footprint) {
+        return footprint.getBodyWidth() * footprint.getBodyHeight();
+    }
+
+    private double getMinusRatio(Footprint footprint) {
+        double ratio = footprint.getBodyHeight() / footprint.getBodyWidth();
+        if (ratio < 1.0) {
+            return ratio;
+        }
+        return 1 / ratio;
+    }
+
     private Location getPickLocation(Camera camera) throws Exception {
+        org.openpnp.model.Package pack = part.getPackage();
+        // FCA @TODO if the part has a big value of height, the focal length is not ridiculous and
+        // the size of
+        // rectangle increase.
+        // For the test purpose, a factor of correction is given with an absolute value, but later,
+        // a method
+        // to find the good constant value (simple thales) must be providen.
+        // @TODO change the method
+        final double correctionFocale = 1.0 - part.getHeight().getValue() * 0.01;
+        final double unitsPerSquarePixels = camera.getUnitsPerPixel().getY()
+                * camera.getUnitsPerPixel().getX() * (correctionFocale * correctionFocale);
+        final double areaPackagePixels = getBodyArea(pack.getFootprint()) / unitsPerSquarePixels;
+        final double ratio = getMinusRatio(pack.getFootprint());
+        // FCA When the rectangle has no area value, give immediatly an error and exit.
+        if (unitsPerSquarePixels == 0) {
+            throw new Exception("Feeder " + getName() + "no dimension for package" + pack.getId());
+        }
+
         // Process the pipeline to extract RotatedRect results
         pipeline.setCamera(camera);
         pipeline.process();
         // Grab the results
         List<RotatedRect> results = (List<RotatedRect>) pipeline.getResult("results").model;
-        if (results.isEmpty()) {
-            throw new Exception("Feeder " + getName() + ": No parts found.");
-        }
-        // Find the closest result
+        logger.debug("Number of rectangles detected : " + results.size());
+        logger.debug("fillter of part : " + part.getName());
+        List<RotatedRect> filteredResults =
+                filterDimensionRotatedRect(results, ratio, areaPackagePixels);
+        logger.debug("Number of rectangles filtered : " + results.size() + " ratio = " + ratio
+                + " sizeArea=" + areaPackagePixels);
+
+        // Find the closest result (area Near than areaPackagePixels)
         results.sort((a, b) -> {
-            Double da = VisionUtils.getPixelLocation(camera, a.center.x, a.center.y)
-                    .getLinearDistanceTo(camera.getLocation());
-            Double db = VisionUtils.getPixelLocation(camera, b.center.x, b.center.y)
-                    .getLinearDistanceTo(camera.getLocation());
+            Double da = Math.abs(a.size.area() - areaPackagePixels);
+            Double db = Math.abs(b.size.area() - areaPackagePixels);
             return da.compareTo(db);
         });
-        RotatedRect result = results.get(0);
+
+        if (filteredResults.isEmpty()) {
+            double errorPcent = 0;
+            if (!results.isEmpty()) {
+                errorPcent = Math.round(Math
+                        .abs((results.get(0).size.area() - areaPackagePixels) / areaPackagePixels));
+            }
+            throw new Exception(
+                    "Feeder " + getName() + ": No parts found. near Area=" + errorPcent + " %");
+        }
+        filteredResults.sort((a, b) -> {
+            Double da = Math.abs(a.size.area() - areaPackagePixels);
+            Double db = Math.abs(b.size.area() - areaPackagePixels);
+            return da.compareTo(db);
+        });
+        RotatedRect result = null;
+        for (RotatedRect rect : filteredResults) {
+            double l2 = VisionUtils.getPixelLocation(camera, rect.center.x, rect.center.y)
+                    .getLinearDistanceTo(camera.getLocation());
+            // FCA @TODO Remove this absolute value. The goal is to keep only the nearest point
+            // between camera and rectangle found.
+            // Else, the camera can move to an other rectangle with a best area and ration each time
+            // of iteration
+
+            if (l2 < 0.5) {
+                if (result == null)
+                    result = rect;
+            }
+        }
+        if (result == null) {
+            // No candidate found, take the first (the best)
+            result = filteredResults.get(0);
+        }
         Location location = VisionUtils.getPixelLocation(camera, result.center.x, result.center.y);
         // Get the result's Location
         // Update the location with the result's rotation
-        location = location.derive(null, null, null, result.angle);
+        double angle = result.angle;
+        // L'angle peut etre de -90 a 90,
+
+        // FCA @TODO remove absolute value, use a rectangle object with isSquare method
+        boolean isSquare = Math.abs(ratio - 1.0) < 0.1; // Si le ratio est <10%, ne plus considerer
+                                                        // le rectangle
+        if (isSquare) {
+            logger.debug("Detection square Angle = " + angle);
+            // A square cannot have an angle high than +/- 45°
+            angle = -angle; // l'angle est toujours negatif
+            if (angle > 45) {
+                angle = (90d - angle);
+            }
+        }
+        else {
+            if ((result.size.width < result.size.height)) // FCA l'angle d'un rectangle 0° = cote
+                                                          // long
+            {
+                angle -= 90;
+                logger.debug("Detection rectangle Angle = " + angle);
+            }
+            angle = Math.abs(angle);
+            if ((angle > 90)) {
+                angle -= 180;
+            }
+        }
+
+        location = location.derive(null, null, null, angle);
         // Update the location with the correct Z, which is the configured Location's Z
         // plus the part height.
         location =
